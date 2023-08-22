3b62bb1ead62f6af7421caa02cd42da7edf10d5b
BakaFT <bakaft@outlook.com>
2023-08-22 14:59:28 +0800

    Assets init

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { RegisterHook_ember } from "./providers/ember";
-import { RegisterHook_xhr } from "./providers/xhr";
-export function init(context: PenguInitContext) {
-  RegisterHook_ember(context)
-  RegisterHook_xhr(context)
-}
-
--- src/types/XhrHook.d.ts
+++
@@ -1,8 +0,0 @@
-import { XhrRequestConfig, XhrResponse } from "ajax-hook"
-
-declare interface XhrHook {
-    matcher: string,
-    preSend: (XhrRequestConfig:XhrRequestConfig) => void,
-    postSend: (XhrResponse:XhrResponse) => void
-}
-
